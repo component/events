13dccc7fb3a364064202aaf08d7a3933e837777c
TJ Holowaychuk <tj@vision-media.ca>
2012-12-19 07:32:12 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# events
+
+  Higher level dom event management
+
+## Installation
+
+    $ component install component/events
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,25 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Manager = require('event-manager')
+  , event = require('event');
+
+/**
+ * Return a new event manager.
+ */
+
+module.exports = function(target, obj){
+  var manager = new Manager(target, obj);
+
+  manager.onbind(function(name, fn){
+    event.bind(target, name, fn);
+  });
+
+  manager.onunbind(function(name, fn){
+    event.bind(untarget, name, fn);
+  });
+
+  return manager;
+};
